9dc8a5250beb625d4864ca759870552de68a0370
isabia <isabellabeatrizn@outlook.com>
2020-02-17 18:05:29 -0300

    Stories widget

--- instagram_uiclone/lib/stories_widget.dart
+++ instagram_uiclone/lib/stories_widget.dart
@@ -1,8 +1,75 @@
 import 'package:flutter/material.dart';
 
 class Stories extends StatelessWidget {
+  final topText = Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Text(
+        "Stories",
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      new Row(
+        children: <Widget>[
+          new Icon(Icons.play_arrow),
+          new Text("Watch All", style: TextStyle(fontWeight: FontWeight.bold))
+        ],
+      )
+    ],
+  );
+
+  final stories = Expanded(
+    child: new Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: new ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (context, index) => new Stack(
+          alignment: Alignment.bottomRight,
+          children: <Widget>[
+            new Container(
+              width: 60.0,
+              height: 60.0,
+              decoration: new BoxDecoration(
+                shape: BoxShape.circle,
+                image: new DecorationImage(
+                    fit: BoxFit.fill,
+                    image: new NetworkImage(
+                        "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2019/05/grumpycat_0008_2.jpg")),
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            ),
+            index == 0
+                ? Positioned(
+                    right: 10.0,
+                    child: new CircleAvatar(
+                      backgroundColor: Colors.blueAccent,
+                      radius: 10.0,
+                      child: new Icon(
+                        Icons.add,
+                        size: 14.0,
+                        color: Colors.white,
+                      ),
+                    ))
+                : new Container()
+          ],
+        ),
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container(height: 10, color: Colors.red);
+    return new Container(
+      margin: const EdgeInsets.all(16.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          topText,
+          stories,
+        ],
+      ),
+    );
   }
 }
