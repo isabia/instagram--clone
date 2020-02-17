96dae16af3a8752ffa996bbbf5a359ba2bc7530b
isabia <isabellabeatriz@outlook.com>
2020-02-17 14:49:00 -0300

    Add App bar

--- instagram_uiclone/lib/feed.dart
+++ instagram_uiclone/lib/feed.dart
@@ -4,7 +4,14 @@ class Feed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Instagram UI clone")),
+      appBar: AppBar(
+          backgroundColor: new Color(0xfff8faf8),
+          leading: IconButton(icon: Icon(Icons.camera_alt), onPressed: () {}),
+          title: Text("Instagram UI clone", textAlign: TextAlign.center),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.send), onPressed: () {}),
+          ]),
     );
   }
 }
