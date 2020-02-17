993c29c664e1f72881357d758b54bed9dc3dfd40
isabia <isabellabeatriz@outlook.com>
2020-02-17 17:05:18 -0300

    Add Feed body

---
+++ instagram_uiclone/lib/feedbody.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'body_list_widget.dart';
+
+class FeedBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        BodyList(),
+      ],
+    );
+  }
+}
