0d0527acba6e7742e3c2e6f37aca527628de8839
isabia <isabellabeatriz@outlook.com>
2020-02-17 17:47:11 -0300

    Create Storie class

---
+++ instagram_uiclone/lib/stories_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Stories extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 10, color: Colors.red);
+  }
+}
