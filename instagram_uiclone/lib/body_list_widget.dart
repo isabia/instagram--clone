87e62f15932e2e1177484b6fd71837ecac17b8c5
isabia <isabellabeatriz@outlook.com>
2020-02-17 17:05:06 -0300

    Add Body list widget

---
+++ instagram_uiclone/lib/body_list_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BodyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemCount: 3, itemBuilder: null);
+  }
+}
